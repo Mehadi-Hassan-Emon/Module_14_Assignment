f6af415053b61ed59cbcc390b40e3d89dacf97b1
HP <mehadimd174@gmail.com>
2025-11-20 18:35:48 +0600

    Added only CRUD folder

--- android/app/src/main/kotlin/com/ostad/b13/flutter_13_new/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.ostad.b13.flutter_13_new
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_13_new/app.dart';
-
-void main() {
-  runApp(MyApp());
-}
